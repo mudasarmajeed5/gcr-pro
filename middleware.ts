c6da9a5be7d39d4cefa557c55d81d7bf8f1f5bfd
mudasarmajeed5 <143939048+mudasarmajeed5@users.noreply.github.com>
2025-09-08 18:30:32 +0500

    update middlware, so browser can access /manifest.json

--- middleware.ts
+++ middleware.ts
@@ -11,9 +11,9 @@ export default auth((req) => {
 
   return NextResponse.next()
 })
-  
+
 export const config = {
   matcher: [
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|sign-in).*)",
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|manifest.json|icon-192x192.png|sign-in).*)",
   ],
 }
