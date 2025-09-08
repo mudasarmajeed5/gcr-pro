9e23c763f3c5b318021b40b78930935f10f64f5b
mudasarmajeed5 <143939048+mudasarmajeed5@users.noreply.github.com>
2025-09-09 01:35:46 +0500

    fix service-worker

--- app/layout.tsx
+++ app/layout.tsx
@@ -211,7 +211,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={poppins.className}>
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
