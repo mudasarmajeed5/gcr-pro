f9a775b33f242938fae1792a1096ffac1b36700f
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2025-10-17 00:29:52 +0500

    Update sitemap with all main routes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,21 @@ module.exports = {
   changefreq: 'daily',
   priority: 0.8,
   sitemapSize: 5000,
+
+  // Add all your main routes manually
+  additionalPaths: async (config) => {
+    const now = new Date().toISOString();
+
+    return [
+      { loc: '/', lastmod: now },
+      { loc: '/sign-in', lastmod: now },
+      { loc: '/assignments', lastmod: now },
+      { loc: '/courses', lastmod: now },
+      { loc: '/get-started', lastmod: now },
+      { loc: '/send-email', lastmod: now },
+      { loc: '/settings', lastmod: now },
+      { loc: '/solver', lastmod: now },
+      { loc: '/manifest.webmanifest', lastmod: now },
+    ];
+  },
 };
